8dacc502206a7756305169b9b6a516d7b9fb4343
gabrielmedsilva <gabrielmedsilva@outlook.com>
2024-09-16 12:35:18 -0300

    feat: Criação so serviço e interface do autor

---
+++ WebApi/WebApi/Services/Autor/IAutorInterface.cs
@@ -0,0 +1,11 @@
+﻿using WebApi.Models;
+
+namespace WebApi.Services.Autor
+{
+    public interface IAutorInterface
+    {
+        Task<ResponseModel<List<AutorModel>>> ListarAutores();
+        Task<ResponseModel<AutorModel>> BuscarAutorPorId(int idAutor);
+        Task<ResponseModel<AutorModel>> BuscarAutorPorIdLivro(int idLivro);
+    }
+}

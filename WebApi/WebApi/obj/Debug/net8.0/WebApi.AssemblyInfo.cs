84e69b85bc0c645aeda9ad752ac5660e539dc07e
gabrielmedsilva <gabrielmedsilva@outlook.com>
2024-09-16 10:53:05 -0300

    rm: Exclui model e controller de exemplo

--- WebApi/WebApi/obj/Debug/net8.0/WebApi.AssemblyInfo.cs
+++ WebApi/WebApi/obj/Debug/net8.0/WebApi.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("WebApi")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+53e13b65ed5f53cdb3e149e4d7028eeb91a6f092")]
 [assembly: System.Reflection.AssemblyProductAttribute("WebApi")]
 [assembly: System.Reflection.AssemblyTitleAttribute("WebApi")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
